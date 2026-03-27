5c0c82d6736267cdb9cd2385e8975925c2a4a709
joaomarcosrd <joao_rd2015@hotmail.com>
2026-03-27 20:25:01 +0000

    Atualização de frase inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
  function home () {
-    return <h1></h1>
+    return <h1>Somos aquilo que repetidamente fazemos. A excelência, portanto, não é um ato, mas um hábito.</h1>
     }
 
  export default home;
